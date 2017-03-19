b0cc6b4631d1c7ee94bc4eeef8285e35a3a25f47
Anna <koshlakova_av@mail.ru>
2017-03-19 21:18:45 +0300

    Fix /start function

--- functions/greet_user.py
+++ functions/greet_user.py
@@ -1,4 +1,12 @@
 def greet_user(bot, update):
 	print('AAA')
 	print(update)
-	bot.sendMessage(update.message.chat_id, text = 'Давай общаться')
+	bot.sendMessage(
+		update.message.chat_id,
+		'Давай общаться!\n\n'
+		'Доступные команды:\n\n'
+		'/wordcount текст\n'
+		'/calc простые выражения 2+2=\n'
+		'/word Сколько будет два плюс три?\n'
+		'/moon Когда будет следующее полнолуние после 01.01.2017?'
+	)
